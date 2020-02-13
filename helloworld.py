d4f11f14c4b5444bfeb28180360a84873f478b2a
Ragas <46344322+grronline@users.noreply.github.com>
2020-02-13 22:58:21 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
+import os
+import time
+USERNAME = os.environ['USERNAME'] 
+LOGINID = os.environ['LOGINID']
 
-print "hello world"
+print "hello world:"+USERNAME+"LOGIID:"+LOGINID
